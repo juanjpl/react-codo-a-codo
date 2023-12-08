a218a0aa6961178448b95af307f52734dc8c05c1
juanjpl <juan_jpl@hotmail.com>
2023-12-08 00:45:47 -0300

    3.0 update

--- src/Hooks/components/methods/Memo/ForwardRef.jsx
+++ src/Hooks/components/methods/Memo/ForwardRef.jsx
@@ -5,15 +5,15 @@ export const ForwardRef = () => {
 
 
     const forwardReftoDiv = useRef('ForwardRef');
-    const forwardRefToSpan = useRef('ForwardRef');
+    const forwardReftoSpan = useRef('ForwardRef');
 
 
-    const handleClick = () => forwardReftoDiv.current.style.backgroundColor = forwardRefToSpan.current.style.backgroundColor= 'green';
+    const handleClick = () => forwardReftoDiv.current.style.backgroundColor = forwardReftoSpan.current.style.backgroundColor= 'green';
 
   return (
     <div className={evenStyle + centerStyle}>
       <button onClick={handleClick}>{forwardReftoDiv.current}</button>
-      <ForwardReftoSpan ref={forwardRefToSpan} />
+      <ForwardReftoSpan ref={forwardReftoSpan} />
       <ForwardReftoDiv ref={forwardReftoDiv} />
     </div>
   );
